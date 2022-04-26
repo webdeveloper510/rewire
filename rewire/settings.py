90eb1cf54e5d9643876428639e3f1b666dda2d9f
webdeveloper510 <developerweb6@gmail.com>
2022-04-26 17:07:15 +0530

    show

--- rewire/settings.py
+++ rewire/settings.py
@@ -189,6 +189,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'testersam3105@gmail.com'
+EMAIL_HOST_USER = 'test12041966@gmail.com'
 EMAIL_HOST_PASSWORD = 'mongodb31'
 EMAIL_PORT = 587
