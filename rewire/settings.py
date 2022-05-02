749eff95ad59ed7582c1e4f98acf5df01cc819cb
webdeveloper510 <developerweb6@gmail.com>
2022-05-02 16:48:08 +0530

    update

--- rewire/settings.py
+++ rewire/settings.py
@@ -224,7 +224,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'testsood981@gmail.com'
-EMAIL_HOST_PASSWORD = 'mongodb31'
+EMAIL_HOST_PASSWORD = 'jivyydbevbsscuoi'
 EMAIL_PORT = 587
 
 
